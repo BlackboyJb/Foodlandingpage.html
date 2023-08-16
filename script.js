f92aa8298ba5960f26590a0feb4ac5e07d30a2c1
Adegoke Odujoko <136864558+BlackboyJb@users.noreply.github.com>
2023-08-16 19:27:47 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const mobile = document.querySelector('.mobile');
+const sidetog = document.querySelector('#sidebar');
+const cancel = document.querySelector('.cancel')
+
+
+mobile.addEventListener('click', function () {
+    mobile.classList.toggle('in-active')
+    sidetog.classList.toggle('active')
+})
+
+
+cancel.addEventListener('click', function () {
+    sidetog.classList.toggle('active')
+    cancel.classList.toggle('in-active')
+
+})
+
+
+
+
